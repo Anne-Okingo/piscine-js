e1453e08d1033d2a1cb18c56c2afecbc26410a4c
aokingo <annekadiso@gmail.com>
2024-09-30 16:07:01 +0300

    feat: add file about constancts, objects,arrys and freezing objects

---
+++ declarations.js
@@ -0,0 +1,29 @@
+const escapeStr = "`\\ /\"'"
+
+const arr = [4,'2']
+
+Object.freeze(arr)
+
+const obj = {
+str : 'Alice',
+num : 254,
+bool : true,
+undef: undefined,
+}
+
+Object.freeze(obj)
+
+const nested = {
+    arr: [
+        3,undefined,'2'
+    ],
+    obj :{
+        str: 'Alice',
+        num: 254,
+        bool: true,
+    }
+}
+
+Object.freeze(nested)
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)
