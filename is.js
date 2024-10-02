e7ec866b391b6698b611fd0eeb5378b5e64112e1
aokingo <annekadiso@gmail.com>
2024-10-02 13:10:07 +0300

    feat: add fuction with typeof concept

--- is.js
+++ is.js
@@ -1,7 +1,4 @@
 
-
-is.num = value => typeof value === 'number' && !isNaN(value); // Checks if value is a number and not NaN
-is.nan = value => Number.isNaN(value); // Checks if value is NaN
 is.str = value => typeof value === 'string'; // Checks if value is a string
 is.bool = value => typeof value === 'boolean'; // Checks if value is a boolean
 is.undef = value => typeof value === 'undefined'; // Checks if value is undefined
@@ -11,4 +8,5 @@ is.obj = value => value !== null && typeof value === 'object' && !Array.isArray(
 is.fun = value => typeof value === 'function'; // Checks if value is a function
 is.truthy = value => !!value; // Checks if value is truthy
 is.falsy = value => !value; // Checks if value is falsy
-
+is.num = value => typeof value === 'number'; // Checks if value is a number (including NaN)
+is.nan = value => Number.isNaN(value); 
