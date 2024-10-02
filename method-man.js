9505436f1c1246feb121e42c04d7eab6651d90f2
aokingo <annekadiso@gmail.com>
2024-10-02 10:01:12 +0300

    feat: add fuction with methods concept

---
+++ method-man.js
@@ -0,0 +1,25 @@
+ function words (str){
+    return str.split(' ')
+ };
+
+ console.log(words('hey you how are you'));
+ function sentence(arr){
+    return arr.join(' ')
+ };
+
+ console.log(sentence([ 'hey', 'you', 'how', 'are', 'you' ]));
+
+ function yell(str){
+    return str.toUpperCase()
+ }
+
+ console.log(yell('hello world'))
+
+ function capitalize(str){
+   return str
+   .split(' ')
+    .map(words => words.charAt(0).toUpperCase()+ words.slice(1).toLowerCase())
+    .join(' ')
+ }
+
+ console.log(capitalize('hello world'))
