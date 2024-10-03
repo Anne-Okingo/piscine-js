08c7d3222c8d2610847f2e2f357fc532f4b1a8f5
anne-okingo <you@example.com>
2024-10-03 22:57:51 +0300

    feat: add function with array.slice concept

--- slicer.js
+++ slicer.js
@@ -1,15 +1,27 @@
 const slice = (array,start= 0, end) =>{
     let newarrary = []
+    if (end === undefined){
+        end = array.length
+    }
     if (start < 0){
         start = array.length + start
     }
     if (end < 0){
         end = array.length + end
     }
+    if(typeof array === 'string'){
+        let slicedstring = ''
     for (let i = start; i < end; i++){
+        slicedstring = slicedstring + array[i]
+    }
+    return slicedstring
+}else if(Array.isArray(array)){
+    for(let i = start; i<end;i++){
         newarrary.push(array[i])
     }
     return newarrary
 }
+return newarrary
+}
 
-// console.log(slice(["Banana", "Orange", "Lemon", "Apple", "Mango"],1,-3))
+console.log(slice(["Banana", "Orange", "Lemon", "Apple", "Mango"],1,-3))
