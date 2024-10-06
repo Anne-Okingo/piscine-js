501d7ac489f513f6043053e6629a14afccbbe82a
anne-okingo <you@example.com>
2024-10-06 06:49:34 +0300

    feat: add fuction

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,5 @@
 function sameAmount(str, regex1, regex2) {
-    const matches1 = str.match(regex1) || []
-    const matches2 = str.match(regex2) || []
-    return matches1.length === matches2.length
+    const matches1 = (str.match(new RegExp(regex1.source, 'g')) || []).length
+    const matches2 = (str.match(new RegExp(regex2.source, 'g')) || []).length
+    return matches1 === matches2
 }
