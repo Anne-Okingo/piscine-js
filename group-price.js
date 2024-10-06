b08a166ed1ec96152af15419e2810728beaebbb3
anne-okingo <you@example.com>
2024-10-06 07:38:52 +0300

    feat:add group-price.js

--- group-price.js
+++ group-price.js
@@ -1,14 +1,13 @@
 function groupPrice(inputString) {
-   
-    const priceRegex = /(\$|\€|\£)(\d+)\.(\d{2})/g;
-    const matches = [];
-    
-    let match;
-    
+    const priceRegex = /(\$|€|£|USD|EUR|GBP)(\d+)\.(\d{2})/g;
+    const matches = []
+
+    let match
+ 
     while ((match = priceRegex.exec(inputString)) !== null) {
-       
-        matches.push([match[0], match[2], match[3]]);
+        
+        matches.push([match[0], match[2], match[3]])
     }
-    
+
     return matches
 }
