9828c915553f8ecc214bd6ad67c4c320bfa97212
anne-okingo <you@example.com>
2024-10-06 06:43:31 +0300

    feat: add fuction

---
+++ same-amount.js
@@ -0,0 +1,8 @@
+function sameAmount(str, regex1, regex2) {
+    const matches1 = str.match(regex1) || []
+    const matches2 = str.match(regex2) || []
+    return matches1.length === matches2.length
+}
+console.log(sameAmount("aabbaa", /a/g, /b/g))
+console.log(sameAmount("abcde", /a/g, /e/g))
+console.log(sameAmount("hello world", /l/g, /o/g))
