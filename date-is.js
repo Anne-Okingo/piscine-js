104735093d12416a747ef9900ca805a4ca550988
Your Name <you@example.com>
2024-10-07 16:16:28 +0300

    feat: add functions with new Date object concept

---
+++ date-is.js
@@ -0,0 +1,21 @@
+function isValid(date){
+    return date instanceof Date && !isNaN(date)
+}
+
+function isAfter(date,date2){
+    return isValid(date) && isValid(date2) && date > date2
+}
+
+function isBefore(date1,date2){
+    return isValid(date1) && isValid(date2) && date1 < date2
+}
+
+
+const newDate = new Date()
+function isFuture(date){
+return isValid(date) && date > newDate
+}
+
+function isPast(date){
+    return isValid(date) && date < newDate
+}
