9160605ad4cf859011cf476d8515486588eed684
Your Name <you@example.com>
2024-10-08 05:00:41 +0300

    feat: add function with leapyear concept

--- count-leap-years.js
+++ count-leap-years.js
@@ -3,7 +3,7 @@ function countLeapYears(date){
     // const newDate = new Date(date)
     let yearNow = date.getFullYear()
 
-    for (let i = 1; i <= yearNow;i++){
+    for (let i = 1; i < yearNow;i++){
         if (i % 4 === 0 && i % 100 !== 0 ||(i % 400 === 0)){
             count++;
         }
