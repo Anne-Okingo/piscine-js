13bd08721fbd884a5b3b2f765abebbd1ec1b663a
Your Name <you@example.com>
2024-10-08 05:42:43 +0300

    feat: add function for the days of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,8 +1,6 @@
-function dayOfTheYear(date){
-    let year = date.getFullYear()
-    let number = 0
-    for (let i = 1; i < year;i++){
-         (i % 4 === 0 && i % 100 !== 0 ||(i % 400 === 0))? number = 366:number = 365
-        }
-    return number
+function dayOfTheYear(date) {
+    const startOfYear = new Date(date.getFullYear(), 0, 1); 
+    const diffInTime = date - startOfYear; 
+    const diffInDays = Math.floor(diffInTime / (1000 * 60 * 60 * 24)) + 1; 
+    return diffInDays;
 }
