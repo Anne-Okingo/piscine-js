6c6c28fd5f5589d0904b0067f21a16ccd40cd42b
Your Name <you@example.com>
2024-10-08 22:00:18 +0300

    feat: add sunny sunday

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,15 +1,22 @@
-const sunnySunday = (date) => {
-    const referenceDate = new Date('0001-01-01');
-    const diffInTime = date.getTime() - referenceDate.getTime();
-    const diffInDays = Math.floor(diffInTime / (1000 * 60 * 60 * 24));
-    const sundaysSkipped = Math.floor(diffInDays / 7);
-    const adjustedDays = diffInDays - sundaysSkipped;
-    const dayOfWeek = adjustedDays % 6;
-    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-    return weekdays[dayOfWeek];
-};
+
+function sunnySunday(date) {
+    const week = [
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday",
+    ];
+    let newdate = new Date(date)
+    const startDate = new Date("0001-01-01T00:00:00");
+    // console.log(startDate)
+    let diffInMilliseconds = newdate - startDate
+    let diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    // console.log(diffInDays)
+
+    return week[diffInDays % 6];
+}
 
 
-
-
+// console.log(sunnySunday('2024-10-31'))
