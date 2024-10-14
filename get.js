ec51c8962ffe1edc01af1b82ed887af2974fc071
Your Name <you@example.com>
2024-10-14 15:58:45 +0300

    feat: add function with reduce concept

--- get.js
+++ get.js
@@ -1,13 +1,23 @@
 function get(src, path) {
-    const keys = path.split('.')
-  
+    const keys = path.split('.');
+
     return keys.reduce((acc, key) => {
-        if (acc && acc.hasOwnProperty(key)) {
-            return acc[key]
+        if (acc === undefined) {
+            return undefined;
         }
-        return undefined; 
-    }, src); 
+
+        if (!isNaN(key)) {
+            key = parseInt(key, 10)
+        }
+
+        if (acc.hasOwnProperty(key) || typeof acc[key] === 'function') {
+            return acc[key];
+        }
+
+        return undefined;
+    }, src);
 }
+
 // const src = { nested: { key: 'peekaboo' } }
 // const path = 'nested.key'
 // console.log(get(src, path)) // -> 'peekaboo'
