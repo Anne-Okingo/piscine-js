1f8d0dbe4fc7bcfd48124c4401b9116eaac0d95d
Your Name <you@example.com>
2024-10-16 06:09:35 +0300

    feat:add function flagger.js

--- flagger.js
+++ flagger.js
@@ -12,13 +12,13 @@ function flags(obj) {
             return `-${key[0]}, --${key}: ${descript}`;
         });
     } else {
-        result.descriptription = Object.keys(obj).map((key) => {
+        result.description= Object.keys(obj).map((key) => {
             let descript = obj[key];
             return `-${key[0]}, --${key}: ${descript}`;
         });
     }
     result.description.length === 0
-        ? (result.descriptription = '')
+        ? (result.description = '')
         : result.description.length === 1
             ? (result.description = result.description[0])
             : (result.description = result.description.join('\n'));
