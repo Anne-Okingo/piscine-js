837c4dcf8b5cc517166034817c8edb5182983ebe
Your Name <you@example.com>
2024-10-18 16:47:22 +0300

    feat: add series function

--- series.js
+++ series.js
@@ -1,12 +1,10 @@
-function series(asyncFunctions) {
-    return async function(...args) {
-        const results = [];
-
-        for (const fn of asyncFunctions) {
-            const result = await fn(...args);
-            results.push(result);
-        }
-
-        return results;
-    };
-}
+async function series(asyncFunctions) {
+    const results = [];
+  
+    for (const asyncFunc of asyncFunctions) {
+     const result = await asyncFunc();
+      results.push(result);
+    }
+  
+    return results;
+  }
